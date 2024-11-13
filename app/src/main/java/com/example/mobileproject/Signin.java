7acaa4d60cc099f2ac091cb544378b6cc3144577
ThinhQuang08 <145360383+ThinhQuang08@users.noreply.github.com>
2024-11-13 15:13:14 +0700

    them project

---
+++ app/src/main/java/com/example/mobileproject/Signin.java
@@ -0,0 +1,19 @@
+package com.example.mobileproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.signin_activity);
+    }
+}
